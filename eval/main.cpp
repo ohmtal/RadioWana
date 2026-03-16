3890b5cfdb655a2d688c35e22c8df22bbf79e486
XXTH <tom@mxplus.de>
2026-03-17 00:40:14 +0100

    skel ready

--- eval/main.cpp
+++ eval/main.cpp
@@ -4,32 +4,50 @@
 //-----------------------------------------------------------------------------
 // RadioWanaEval
 //-----------------------------------------------------------------------------
+#include <SDL3/SDL_main.h>
 
 #include <fluxMain.h>
-#include <SDL3/SDL_main.h>
+#include <gui/fluxGuiGlue.h>
+#include <gui/ImFlux.h>
+#include <imgui.h>
 #include <miniaudio.h>
 #include <httplib.h>
 
 class RadioWanaEval : public FluxMain
 {
     typedef FluxMain Parent;
+
 private:
-    bool Initialize() override    { if (!Parent::Initialize()) return false;}
-    void Deinitialize() override  { Parent::Deinitialize(); }
-    void onMouseButtonEvent(SDL_MouseButtonEvent event) override  {}
-    void onDraw() override {}
+     FluxGuiGlue* mGuiGlue;
+public:
+
+    bool Initialize() override    {
+        if (!Parent::Initialize()) return false;
+        mGuiGlue = new FluxGuiGlue(true);
+        if (!mGuiGlue->Initialize())
+            return false;
+        return true;
+    }
+    void Deinitialize() override  {
+        SAFE_DELETE(mGuiGlue);
+        Parent::Deinitialize();
+    }
+    void onEvent(SDL_Event event) override{ mGuiGlue->onEvent(event);}
     void onKeyEvent(SDL_KeyboardEvent event) override  {
-        if (event.type == SDL_EVENT_KEY_UP && event.key = SDLK_ESCAPE) {
+        if ((event.type == SDL_EVENT_KEY_UP) && (event.key = SDLK_ESCAPE)) {
             TerminateApplication();
         }
     }
     void onDrawTopMost() override
     {
+        mGuiGlue->DrawBegin();
         ImGui::SetNextWindowSizeConstraints(ImVec2(800.0f, 600.f), ImVec2(FLT_MAX, FLT_MAX));
         if (ImGui::Begin("RadioWana")) {
 
+
         }
-        ImGui::End;
+        ImGui::End();
+        mGuiGlue->DrawEnd();
 
     }
 
